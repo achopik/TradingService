a5ffd3075c6b92f417ace179af62e550b0e23f08
achopik <a-chopik@mail.ru>
2020-12-16 18:11:07 +0300

    Implemented serializers

---
+++ sources/trading/serializers.py
@@ -0,0 +1,87 @@
+
+from rest_framework import serializers
+from trading.models import (
+    Currency,
+    Item,
+    Price,
+    WatchList,
+    Trade,
+    Offer,
+    Inventory,
+    User,
+    Balance
+)
+
+
+class CurrencySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Currency
+        fields = '__all__'
+
+
+class ItemSerializer(serializers.ModelSerializer):
+
+    currency = serializers.SlugRelatedField(queryset=Currency.objects.all(), slug_field='code', many=False)
+
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+
+class PriceSerializer(serializers.ModelSerializer):
+
+    item = serializers.SlugRelatedField(queryset=Item.objects.all(), slug_field='code', many=False)
+    currency = serializers.SlugRelatedField(queryset=Currency.objects.all(), slug_field='code', many=False)
+
+    class Meta:
+        model = Price
+        fields = '__all__'
+
+
+class BaseSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='id', many=False)
+    item = serializers.SlugRelatedField(queryset=Item.objects.all(), slug_field='name', many=False)
+
+
+class WatchListSerializer(serializers.ModelSerializer):
+
+    user = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='id', many=False)
+    item = serializers.SlugRelatedField(queryset=Item.objects.all(), slug_field='id', many=True, write_only=True)
+
+    class Meta:
+        model = WatchList
+        fields = '__all__'
+
+
+class OfferSerializer(BaseSerializer):
+
+    class Meta:
+        model = Offer
+        fields = '__all__'
+
+
+class InventorySerializer(BaseSerializer):
+
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+
+class TradeSerializer(serializers.ModelSerializer):
+
+    item = serializers.SlugRelatedField(queryset=Item.objects.all(), slug_field='code', many=False)
+
+    class Meta:
+        model = Trade
+        fields = '__all__'
+
+
+class BalanceSerializer(serializers.ModelSerializer):
+
+    user = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='id', many=False)
+    currency = serializers.SlugRelatedField(queryset=Currency.objects.all(), slug_field='code', many=False)
+
+    class Meta:
+        model = Balance
+        fields = '__all__'

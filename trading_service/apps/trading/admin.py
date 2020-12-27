3d2dc2edafb754211d352ce0db9576800e66e508
achopik <a-chopik@mail.ru>
2020-12-27 20:12:04 +0300

    Apps moved in their own directory

--- trading_service/apps/trading/admin.py
+++ trading_service/apps/trading/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from trading.models import (
+from .models import (
     Balance,
     Currency,
     Inventory,
--- trading_service/core/settings.py
+++ trading_service/core/settings.py
@@ -12,20 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import datetime
 import os
+import sys
 from pathlib import Path
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(BASE_DIR, "apps"))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=0)
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
